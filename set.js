feaa5a390ffa7ef9035e2cdceb4d68792c6faff6
djalega8000 <139915759+djalega8000@users.noreply.github.com>
2023-08-25 10:19:02 +0000

    Create set.js
    Première création

---
+++ set.js
@@ -0,0 +1,44 @@
+const fs = require('fs-extra');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || 'zokk',
+    PREFIXE: process.env.PREFIXE,
+    NOM_OWNER: process.env.NOM_OWNER || "Zokou-Md",
+    NUMERO_OWNER : process.env.NUMERO_OWNER,              
+    LECTURE_AUTO_STATUS: process.env.LECTURE_AUTO_STATUS || "non", APILOLHUMAIN: process.env.APILOLHUMAIN,
+    NOM_BOT: process.env.NOM_BOT || "Ƶ𝓞ｋØ𝓊-2.0",
+    IMAGE_MENU: process.env.IMAGE_MENU || "https://wallpapercave.com/uwp/uwp3860299.jpeg",
+    TELECHARGER_AUTO_STATUS: process.env.TELECHARGER_AUTO_STATUS || 'non',
+    MODE: process.env.MODE_PUBLIC,
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
+    /* new Sequelize({
+     dialect: 'sqlite',
+     storage: DATABASE_URL,
+     logging: false,
+})
+: new Sequelize(DATABASE_URL, {
+     dialect: 'postgres',
+     ssl: true,
+     protocol: 'postgres',
+     dialectOptions: {
+         native: true,
+         ssl: { require: true, rejectUnauthorized: false },
+     },
+     logging: false,
+}),*/
+};
+let fichier = require.resolve(__filename);
+fs.watchFile(fichier, () => {
+    fs.unwatchFile(fichier);
+    console.log(`mise à jour ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
+});
