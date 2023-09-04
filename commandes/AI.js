a1c13cf8d164d3749fff35a2d30e798f9247e603
djalega8000 <139915759+djalega8000@users.noreply.github.com>
2023-09-04 06:51:06 +0000

    Add files via upload
    Nouvelle création

--- commandes/AI.js
+++ commandes/AI.js
@@ -1,5 +1,7 @@
-/*const { zokou } = require('../framework/zokou');
-const { openai } = require('openai');
+const { zokou } = require('../framework/zokou');
+const tr =require("translate-google-api")
+const deepai=require("deepai")
+/*const { openai } = require('openai');
 
 zokou({ nomCom: "gpt", categorie: "AI" }, async (dest, zk, commandeOptions) => {
   const { arg, repondre } = commandeOptions;
@@ -12,40 +14,3 @@ zokou({ nomCom: "gpt", categorie: "AI" }, async (dest, zk, commandeOptions) => {
   await doReact("✅");
 
   const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-  async function generateResponse(prompt, retries = 2) {
-    try {
-      const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: prompt }],
-      });
-
-      return completion.data.choices[0].message.content.trim();
-    } catch (error) {
-      if (
-        error.response &&
-        error.response.status === 429 &&
-        retries > 0
-      ) {
-        const retryAfter =
-          error.response.headers["retry-after"] * 1000 || 5000;
-        repondre(
-          `Limite de taux dépassée. Réessayez dans ${
-            retryAfter / 1000
-          } secondes...`
-        );
-        await sleep(retryAfter);
-        return generateResponse(prompt, retries - 1);
-      } else {
-        console.error(error);
-        await doReact("❌");
-        return "Une erreur s'est produite lors de la génération de la réponse - limite d'utilisation de l'API dépassée ou clé d'API incorrecte.";
-      }
-    }
-  }
-
-  const prompt = arg.join(" ");
-  const response = await generateResponse(prompt);
-  
-  repondre(` ${response}`);
-}); */
---
+++ commandes/proprio.js
@@ -0,0 +1,31 @@
+const {zokou}=require("../framework/zokou")
+
+
+
+
+
+
+
+zokou({nomCom:"redemarrer",categorie:"Mods",reaction:"👨🏿‍💼"},async(dest,z,com)=>{
+
+
+  
+const{repondre,ms,dev,superUser}=com;
+
+  if(!dev ||!superUser)
+  {
+    return repondre("Cette commande est réservée au propriétaire du bot");
+  }
+
+  const {exec}=require("child_process")
+
+    repondre("* Redémarrage du bot en cour ...*");
+
+  exec("pm2 restart all");
+  
+
+  
+
+
+
+})
