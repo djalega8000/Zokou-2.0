518e12206b00784c2414e28560d9dee0d60780c2
Faouz995 <105295384+Faouz995@users.noreply.github.com>
2023-09-29 20:32:10 +0000

    nouveau menu

--- commandes/menu.js
+++ commandes/menu.js
@@ -16,7 +16,6 @@ zokou({ nomCom: "menu", categorie: "Général" }, async (dest, zk, commandeOptio
         mode = "privé";
     }
     
-    var emoji = { "Général": "🌐", "Logo": "🎨", "Hentai": "🔥", "Weeb": "🌸", "Recherche": "🔍", "Conversion": "🌟", "Groupe": "♻️", "Autre": "🪖" };
 
     cm.map(async (com, index) => {
         if (!coms[com.categorie])
@@ -28,48 +27,62 @@ zokou({ nomCom: "menu", categorie: "Général" }, async (dest, zk, commandeOptio
     moment.tz.setDefault('asia/karachi ').locale("id");
     const date = moment.tz("asia/karachi").format("DD/MM/YYYY");
 
-  let infoMsg = `
-╔════---------
-║    Préfixe : ${s.PREFIXE}
-║    Owner : ${s.NOM_OWNER}
-║    Mode : ${mode}
-║    Commandes : ${cm.length}
-║    Date : ${date}
-║    Heure : ${temps}
-║    Mémoire : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
-║    Plateforme : ${os.platform()}
-║    Développeurs : Djalega++ 
-║     & Luffy
-╚════--------------- \n\n`;
+  let infoMsg = `╔═════ஜ۩۞۩ஜ═══╗
+║📌 *Préfixe* -> ${s.PREFIXE}
+║📌 *Owner* -> ${s.NOM_OWNER}
+║📌 *Mode* -> ${mode}
+║📌 *Commandes* -> ${cm.length}
+║📌 *Date* -> ${date}
+║📌 *Heure* -> ${temps}
+║📌 *Mémoire* -> ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+║📌 *Plateforme* -> ${os.platform()}
+║📌 *Développeurs* -> Djalega++ 
+║ & ᚛M๏𝓷keℽ D Lบffy᚜
+╚═════ஜ۩۞۩ஜ═══╝\n\n`;
     
-    let menuMsg = `
-👋 salut ${nomAuteurMessage} 👋
-Je suis *${s.NOM_BOT}*, un bot développé par *La team Zokou*.
-🔰 Mon préfixe : ${prefixe}
-**Voici la liste de mes commandes :**
-`;
+    let menuMsg = `*👋 salut ${nomAuteurMessage} 👋*
+Je me nomme *${s.NOM_BOT}*, un bot développé par *La team Zokou*.
+
+*Voici la liste de mes commandes :*
+\n`;
 
     for (const cat in coms) {
-        menuMsg += `
-*━━━〈  ${cat} 〉━━━*`;
+        menuMsg += `*╚»★«╝${cat}╚»★«╝*\n`;
         for (const cmd of coms[cat]) {
-            menuMsg += `
-- ${cmd}`;
+            menuMsg += `🔹 ${prefixe}${cmd}\n`;
         }
     }
 
-    menuMsg += `\n\n
-🔰 Pour utiliser une commande, tapez "${prefixe}<nom de la commande>".
-🔰 Fait avec node.js || Zokou_2.0-MD
+    menuMsg += `\n➖➖➖➖➖➖➖➖➖
+✔️  *Pour utiliser une commande, tapez "${prefixe}<nom de la commande>"*.
+✔️  *Fait avec node.js || Zokou_2.0-MD*
 `;
 
-   // var link = s.IMAGE_MENU;
+   var lien = botpic();
 
+   if (lien.match(/\.(mp4|gif)$/i)) {
     try {
-        zk.sendMessage(dest, { image: { url: botpic() }, caption:infoMsg + menuMsg, footer: "Je suis *Zokou-MD*, développé par Djalega++" }, { quoted: ms });
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Zokou-MD*, développé par Djalega++" }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
         repondre("🥵🥵 Menu erreur " + e);
     }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Zokou-MD*, développé par Djalega++" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    repondre(lien)
+    repondre("Le lien ne se termine ni par .mp4 ou .gif ni par .jpeg , jpg ou .png");
+    
+}
+
 });
