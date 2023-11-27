3e2d3e69f71f7bde3a498b56032d7c13bc4a316e
djalega8000 <139915759+djalega8000@users.noreply.github.com>
2023-11-27 19:02:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ setTimeout(() => {
             const verifAdmin = verifGroupe ? admins.includes(auteurMessage) : false;
             var verifZokouAdmin = verifGroupe ? admins.includes(idBot) : false;
             /** ** */
-            await zk.sendPresenceUpdate("recording");
+            await zk.sendPresenceUpdate("composing");
             /** ***** */
             const arg = texte ? texte.trim().split(/ +/).slice(1) : null;
             const verifCom = texte ? texte.startsWith(prefixe) : false;
