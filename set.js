2f4a222d9e8da7a012fb8301559360f44c5c1e43
djalega8000 <139915759+djalega8000@users.noreply.github.com>
2023-11-29 06:55:33 +0000

    Update set.js
    Ajout de ETAT

--- set.js
+++ set.js
@@ -8,6 +8,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || 'zokk',
+                  ETAT:process.env.ETAT,
     PREFIXE: process.env.PREFIXE,
     NOM_OWNER: process.env.NOM_OWNER || "Zokou-Md",
     NUMERO_OWNER : process.env.NUMERO_OWNER,              
