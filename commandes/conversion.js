ac111b1000cde2f24f5b8d44ac55d6cf3f25fa70
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-10 23:25:42 +0000

    Update conversion.js

--- commandes/conversion.js
+++ commandes/conversion.js
@@ -286,29 +286,36 @@ zokou({nomCom:"url",categorie: "Conversion", reaction: "👨🏿‍💻"},async(
     const video = await zk.downloadAndSaveMediaMessage(mediamsg);
     // Créer un objet FormData
   const data = new FormData();
-  data.append('image', fs.createReadStream(video));
-
-  // Configurer les en-têtes
-  const clientId = 'b40a1820d63cd4e'; // Remplacez par votre client ID Imgur
-  const headers = {
-    'Authorization': `Client-ID ${clientId}`,
-    ...data.getHeaders()
-  };
-
-  // Configurer la requête
-  const config = {
-    method: 'post',
-    maxBodyLength: Infinity,
-    url: 'https://telegra.ph,
-    headers: headers,
-    data: data
-  };
+  data.append('video', fs.createReadStream(video)); 
+  const url =`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Secktor-Md+Bot&author_name=SamPandey001&content=[%7B"tag":"p","children":["${mediamsg.replace(/ /g, '+')}"]%7D]&return_content=true`;
+  let {data} =await axios.get(Url);
 
     const response = await axios(config);
-    const videoUrl = response.data.data.link;
+    const videoUrl = response.data.result.link;
     console.log(videoUrl) ;
 
        repondre(videoUrl)
+
+
+  let a = citel.quoted ? citel.quoted.text : citel.text;
+    let apiToken = 'd3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722'; // Replace with your Telegraph API token
+    let apiUrl = `https://api.telegra.ph/createPage?access_token=${apiToken}&title=Secktor-Md+Bot&author_name=SamPandey001&content=[%7B"tag":"p","children":["${a.replace(/ /g, '+')}"]%7D]&return_content=true`;
+
+    try {
+        let { data } = await axios.get(apiUrl);
+        if (data && data.result) {
+            let title = util.format(data.result.title);
+            let url = util.format(data.result.url);
+            let stylishReply = `*Paste created on Telegraph*\nName:- ${title}\nUrl:- ${url}`;
+            return citel.reply(stylishReply);
+        } else {
+            return citel.reply('Error creating paste on Telegraph.');
+        }
+    } catch (error) {
+        console.error('Error creating paste:', error);
+        return citel.reply('Error creating paste on Telegraph.');
+    }
+});
 
      } else if (msgRepondu.stickerMessage) {
     mediamsg = msgRepondu.stickerMessage ;
