1a51fbf3f3c3e475d4e7da745d896d9cd128b840
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-15 21:37:40 +0000

    Update gpt.js

--- commandes/gpt.js
+++ commandes/gpt.js
@@ -18,29 +18,31 @@ async function getChatGPTResponse(question) {
         },
       }
     );
-    
+
     const data = response.data;
     return data.choices[0].text.trim();
+  } catch (error) {
+    console.error('Erreur lors de la requête à l\'API OpenAI:', error.message);
+    throw error; // Rethrow the error to handle it outside this function if needed
   }
 }
 
 zokou({ nomCom: "gpt", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
   const { repondre, arg } = commandeOptions;
 
-  if (!arg || arg.length === 0) {
-    return repondre("Veuillez poser votre question.");
-  }
-
-  const question = arg.join(' ');
-
   try {
+    if (!arg || arg.length === 0) {
+      return repondre("Veuillez poser votre question.");
+    }
+
+    const question = arg.join(' ');
     const reponse = await getChatGPTResponse(question);
-    
+
     if (reponse) {
       repondre(reponse);
     } else {
-      console.error('Erreur lors de la requête à l\'API OpenAI:');
-      return repondre('Une erreur s\'est produite lors du traitement de votre demande.');
+      console.error('Erreur lors de la requête à l\'API OpenAI.');
+      repondre('Une erreur s\'est produite lors du traitement de votre demande.');
     }
   } catch (e) {
     console.error('Erreur générale :', e);
