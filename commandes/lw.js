bfd954734ece4c576108cfd56b0e44d818682a7a
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-15 18:53:15 +0000

    Update lw.js

--- commandes/lw.js
+++ commandes/lw.js
@@ -1,1 +1,330 @@
-const {zokou} = require(..farmork/zokou)
+zokou({ nomCom: "lw", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+  const { repondre, arg } = commandeOptions;
+
+  if (!arg || arg.length === 0) {
+    return repondre("*INFO*\n*");
+  }
+
+  const option = arg.join(' ');
+
+if (option = '') {
+    repondre(`🔷 *🎟️ARIA SHOP🛍️🛒*
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+Fiche de transaction :
+
+*👤𝐉𝐨𝐮𝐞𝐮𝐫*:
+*🛍️𝐎𝐩𝐞𝐫𝐚𝐭𝐢𝐨𝐧*:
+*🛒𝐃𝐞𝐩𝐞𝐧𝐬𝐞𝐬*:...
+*💰𝐒𝐨𝐥𝐝𝐞 𝐫𝐞𝐬𝐭𝐚𝐧𝐭*:
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+*🛒𝐑𝐞𝐜̧𝐮𝐬*:
+
+
+
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+   🔹 *Merci☺️✌🏼* 🛍️`)
+} else if(option = '') {
+    repondre(`*⚔️(PROGRESSION %)🎮*
+░░░░░░░░░░░░░░░░░░░pour les missions &Quête  uniquement (fight)
+▔▔▔▔▔▔▔▔▔▔▔▔▔
+               * statistiques *
+⚔️MSS:
+🛡:  % ❤️:  % 🔋:  % 
+
+⚔️PLAYEUR:
+🛡:  % ❤️:  % 🔋:  % 
+
+⚔️PLAYEUR:
+🛡:  % ❤️:  % 🔋:  % 
+
+⚔️PLAYEUR:
+🛡:  % ❤️:  % 🔋:  % 
+
+⚔️PLAYEUR:
+🛡:  % ❤️:  % 🔋:  % 
+
+⚔️PLAYEUR:
+🛡:  % ❤️:  % 🔋:  % 
+
+░░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+    *⚔️LEGENDARY.WARS🎮*`)
+} else if(option = '') {
+    repondre (`*⚔️joueur (LEGENDARY.WARS)   🎮*
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔
+               * DISCUTIONS *
+⚔️
+
+  
+
+   
+░░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+              *⚔️LEGENDARY.WARS🎮*`) 
+} else if(option = '') {
+    repondre(`*⚔️INFORMATIONS (VICTOIRE&RECOMPENSES💰)🎮*
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔* NOM DU JOUEUR*:
+⚔️
+*RÉCOMPENSES*:
+⚔️
+ *Types de mission *
+⚔️
+*Lieux de la mission *
+⚔️
+*Durée de la mission *:♾️
+░░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+              *⚔️LEGENDARY.WARS🎮*`)
+} else if(option = '') {
+    repondre(`*⚔️ARMES(.             )🎮*
+░░░░░░░░░░░░░░░░░░░░*propriétaire* :
+
+ *Dégât *:
+*Quantité *:
+*Prix*:
+        *CAPACITÉ PROPRE*
+
+MODE D'emploi :
+  
+⚔️
+   
+░░░░░░░░░░░░░░░░░░░░
+
+             *⚔️LEGENDARY.WARS🎮*`)
+} else if(option = '') {
+    repondre(`*⚔️LEGENDARY WARS🎮*       
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+⚔️.  *JOUEUR*: 🫀:100%  🔋:100% ❤️:100% 🛡:30%            
+                                   ~  *🆚*  ~
+⚔️.*JOUEUR*: 🫀:100%  🔋:100% ❤️:100%:.🛡:30%                            
+ ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+ *🚫𝐇𝐚𝐧𝐝𝐢𝐜𝐚𝐩𝐞*: l
+ *🏞️ 𝐀𝐢𝐫 𝐝𝐞 𝐜𝐨𝐦𝐛𝐚𝐭*: 50𝙢
+ *🦶🏼𝐃𝐢𝐬𝐭𝐚𝐧𝐜𝐞 𝐢𝐧𝐢𝐭𝐢𝐚𝐥𝐞*📌: 5m
+ *⌚𝐋𝐚𝐭𝐞𝐧𝐜𝐞*: 8mins
+ *🌍𝐄𝐧𝐯𝐢𝐫𝐨𝐧𝐧𝐞𝐦𝐞𝐧𝐭*: 
+ ░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+  *⚠𝐋𝐀𝐓𝐄𝐍𝐂𝐄 𝐄𝐓 𝐑𝐄𝐓𝐀𝐑𝐃🚫❌:* En cas de retard 
+▪Pavé refusé sans bonne raison immobilité. +1min acceptable et un retard ne peu que être tolérer une seule fois dans un combat. 
+▪OFF🚫Après 10mins sans signe de connexion ❌
+
+
+
+🆚 *𝐂𝐎𝐍𝐃𝐈𝐓𝐈𝐎𝐍𝐒 𝐃𝐄 𝐕𝐈𝐂𝐓𝐎𝐈𝐑𝐄𝐒*
+▪Victoire Easy,No diff✅: 2-0
+▪Victoire Mid Diff✅: 1-0 - 50%🫀
+▪Victoire High Extreme Diff✅: 2-1 - 60%PV❤️
+▪Débat à tords: 🚫risque de grosses amendes 
+▪Insulte sur l'arbitre: 🚫risque de Ban. 
+
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+  *Vous avez 8️⃣ tours max pour finir votre Adversaire! DECISION*`)
+} else if(option = '') {
+    repondre(`*⚔️RÈGLES GÉNÉRALES 🎮*
+░░░░░░░░░░░░░░░░
+        *Attaque à main nu *:
+
+ les attaques simple (coup de poing ou de pied etc) sur une autre formes de vie biologiques que des humains elle retire 5%❤️ mais sur des humains elle retire 10%❤️ et dans des endroits sensible comme la tête et les côtes elle retire 10%❤️pour les autres espèces et 20%❤️ sur les humains..
+   *Attaque avec des armes:*
+ 
+Pour les pv retirer par une armes elle varie selon l'arme mais en cas de membre sectionné pour les autres espèces comme pour les humains elle retire 30%❤️par coup et entraîne un saignements qui coûte 5%❤️à chaque tours 
+
+*Récompense mission /quêtes et duel*:
+
+*Duel*:10000🪪+3pts dans le classement +5xp🔮
+*Mission* :elle peuvent varier mais si on ne précise pas elle est de 10000🪪+des point de compétence (pc)qui varie par mission.+10xp🔮
+*Quête *:.?.?.🪪+..?.?.(Pc)elle varie selon la difficulté elle sont attribués par le modos +10xp🔮
+
+         *Titres et rang*:
+▔▔▔▔▔▔▔▔▔▔▔▔▔
+  pour être :
+🔥aspirant révolutionnaire :il faut avoir minimum 100xp🔮
+🔥un semi-revolutionaire:il faut avoir minimum 200xp🔮
+🔥révolutionaire:il faut avoir minimum 300xp🔮
+🔥révolutionnaire expérimenté :il faut avoir minimum 400xp
+🔥aspirant conquérant :il faut avoir minimum 500xp🔮
+🔥🔥conquérant :il faut avoir  tuer l'actuel conquérant 💀 
+    * instructions panel *:
+Le nombre d'action par panel est de 2 mais vous pouvez aller à 3 en combinant 2action (combo) et lorsque la première action de votre panel ou encore la séquence 1 est arrêté la séquences 2 n'a pas lieu..vous pouvez combiner une action avec un déplacement (combo)
+NB : de base l'équipement de chacun se compose de 2 dagues et vous avez pour ranf/titre (débutant)
+          *Armes & utilisation *
+Chaque armes possède une capacité propre mais le point commun entre toutes est quels sont capables d'infliger soi des dégâts ❤️moyen(10%❤️) ou des dégâts important...ainsi la vitesse de lancée d'un projectile varie selon la force (vitesse d'un coup =vitesse de lancer) et la porter d'un projectile est égal à votre Agilité mais pour chaque distance (mètres)supplémentaire effectué par votre projectile hors de votre portée selon votre Agilité le projectile perd en vitesse de 1m/s pour chaque mètres supplémentaire 
+*.         Autres informations *
+
+Il est possible de booster à tout moment mais cela n'augmentera en aucun cas vos aptitudes physique ou autre..il sert juste à se ressaisir après avoir été sonner par un coup ou une attaque,chaque armes possède une capacité propre et dès que vous utilisez cette capacité vous perdez 20🔋 pour chaque utilisation à condition de réunir les conditions pour l'activation de ses capacités...
+░░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+              *⚔️LEGENDARY.WARS🎮*`)
+} else if(option = '') {
+    repondre(`*⚔️MONSTERS(MSS)🎮*
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+Life:     ❤️%
+Force:    🦾%
+Vitesse:   🏃%
+ défense :   %
+░░░░░░░░░░░░░░░░░░░
+💬𝗖𝗵𝗮𝘁:
+▔▔▔▔▔▔▔▔▔▔▔▔▔
+             *CARACTÈRE *:
+
+
+
+
+░░░░░░░░░░░░░░░░░░░
+
+       *⚔️LEGENDARY.WARS🎮*`)
+} else if(option = '') {
+    repondre(`*⚔️STATISTIQUES 🎮*
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+                 FORCE🦾:
+ ⚔️si pc <ou =10 la vitesse d'un coup (1m/s)
+⚔️si pc<ou=20 la vitesse d'un coup (1,5m/s)
+⚔️si pc<ou=40 la vitesse d'un coup (2m/s)
+⚔️si pc<ou=60la vitesse d'un coup (2,5m/s)
+⚔️si pc<ou=80 la vitesse d'un coup (3m/s)
+⚔️si pc<ou=100 la vitesse d'un coup(3m/s)
+░░░░░░░░░░░░░░░░
+*.          Vitesse de déplacement /temps de réaction* :
+
+🏃si pc<ou=10 vitesse (2m/s)/TR=1,5s
+🏃si pc<ou=35 vitesse (3m/s):TR=1s
+🏃si pc <ou=60 vitesse (4m/s)/TR=0,5s
+🏃si pc<ou=80 vitesse (5m/s)/TR=0,5s
+🏃si pc<ou=90 vitesse (6m/s)/TR=simultanément 
+  🏃si pc<ou=100 vitesse (instantané)/TR =+0,5s en avance 
+░░░░░░░░░░░░░░░░
+            *AGILITÉ /action simultané :*
+   🎯si pc <ou=30 tout ce qu'on vise à 5m est sûr d'atteinte /AS(0)
+🎯si pc<ou=60 tout ce qu'on vise à 10m est sûr d'atteinte /AS(0)
+🎯si pc<ou=90 tout ce qu'on vise à 20m est sûr d'atteinte/AS(0)
+🎯si pc<ou=100 tout ce qu'on vise à 25m est sûr d'atteinte/AS(3) 
+AS=action simultané 
+TR=temps de réaction 
+░░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+              *⚔️LEGENDARY.WARS🎮*`)
+} else if(option = '') {
+    repondre(`*⚔️FICHE DE PERSO🎮*
+░░░░░░░░░░░░░░░░░░░
+Pseudo du joueur:
+Âge :
+Sexe:
+Race :humaine
+Appartenance à une faction:
+Nom de la faction:
+                 
+                Équipement :
+➖2 dagues
+➖
+➖
+➖
+➖
+-Nombre de 🪪:10000🪪
+-Nombre de victoire:
+-Nombre de⏱:
+-Rang/titre:débutant
+-Nombre de défaite :
+▔▔▔▔▔▔▔▔▔▔▔▔▔
+NB:Il est possible de vendre certains équipement contre des 🪪dans la boutique pour-50% du prix d'achat originel
+*Date d'actualisation* :.../..../...23
+              *⚔️LEGENDARY.WARS🎮*`)
+} else if(option = '') {
+    repondre(`*⚔️INFORMATIONS   🎮*
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔
+                  * À SAVOIR *
+⚔️
+
+  
+
+   
+░░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+              *⚔️LEGENDARY.WARS🎮*`)
+} else if(option = '') {
+    repondre(`*⚔️PNJ(LEGENDARY.WARS)   🎮*
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔
+               * DISCUTIONS *
+⚔️
+
+  
+
+   
+░░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+              *⚔️LEGENDARY.WARS🎮*`)
+} else if(option = '') {
+    repondre(`⚔️ *🆚 𝐃𝐔𝐄𝐋&MISSION🎮*
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+⚔️  *𝐂𝐨𝐧𝐜𝐥𝐮𝐬𝐢𝐨𝐧🎮*:
+
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+⚔️ *𝐀𝐫𝐛𝐢𝐭𝐫𝐞🛂* : 
+⚔️ *𝐀𝐫𝐞𝐧𝐞🎙️*: 
+
+═══════════
+  *⚔️LEGENDARY WARS 🎮*`)
+  } else if(option='') {
+    repondre(`*⚔️répartition (pc)🎮*
+░░░░░░░░░░░░░░░░░░░NOM du joueur :
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+Force: 000 /100%
+Vitesse/TR : 000 /100%
+ Agilité /AS: 000 /100%
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔
+             *CARACTÈRE *:
+
+
+
+Date d'actualisation :.../.../....23
+░░░░░░░░░░░░░░░░░░░
+
+       *⚔️LEGENDARY.WARS🎮*`)
+  } else if(option='') {
+    repondre(`*⚔️PANEL CONTRÔLE  🎮*
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+𝗗𝗶𝘀𝘁𝗮𝗻𝗰𝗲: 5m
+ 
+
+💬𝗖𝗵𝗮𝘁:
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+  
+⚔️
+   
+░░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+              *⚔️LEGENDARY.WARS🎮*`)
+  } else if(option='') {
+    repondre(`*⚔️(PROGRESSION %)🎮*
+░░░░░░░░░░░░░░░░░░░pour les missions &Quête  uniquement (fight)
+▔▔▔▔▔▔▔▔▔▔▔▔▔
+               * statistiques *
+⚔️MSS:
+🛡:  % ❤️:  % 🔋:  % 
+⚔️PLAYEUR:
+🛡:  % ❤️:  % 🔋:  % 
+░░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+    *⚔️LEGENDARY.WARS🎮*`)
+} else if(option = undefined) {
+    repondre(`option introuvable`)
+   
+  } catch (e) {
+    console.error('Erreur générale :', e);
+    repondre("une erreur est survenue.");
+  }
+});
