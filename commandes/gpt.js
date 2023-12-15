bd21d26a5aad0f5d2bbc67117f84080ed8138bdf
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-15 23:44:32 +0000

    Update gpt.js

--- commandes/gpt.js
+++ commandes/gpt.js
@@ -12,33 +12,28 @@ zokou({ nomCom: "gpt", reaction: "📡", categorie: "IA" }, async (dest, zk, com
     const question = arg.join(' ');
     const OPENAI_API_KEY = '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';
 
-    try {
-      const response = await axios.post(
-        'https://api.openai.com/v1/engines/completions',
-        {
-          prompt: question,
-          max_tokens: 200,
+    const response = await axios.post(
+      'https://api.openai.com/v1/engines/gpt-3.5-turbo/completions',
+      {
+        prompt: question,
+        max_tokens: 200,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${OPENAI_API_KEY}`,
         },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${OPENAI_API_KEY}`,
-          },
-        }
-      );
-
-      const data = response.data;
-      const data0 = data.choices[0].text.trim();
-
-      if (data0) {
-        repondre(data0);
-      } else {
-        console.error('Erreur lors de la requête à l\'API OpenAI.');
-        repondre('Une erreur s\'est produite lors du traitement de votre demande.');
       }
-    } catch (error) {
-      console.error('Erreur lors de la requête à l\'API OpenAI:', error.message);
-      throw error; // Rethrow the error to handle it outside this function if needed
+    );
+
+    const responseData = response.data;
+
+    if (responseData.choices && responseData.choices.length > 0) {
+      const reponseChatGPT = responseData.choices[0].text.trim();
+      repondre(reponseChatGPT);
+    } else {
+      console.error('Réponse invalide de l\'API OpenAI:', responseData);
+      repondre("Une erreur s'est produite lors du traitement de votre demande.");
     }
   } catch (e) {
     console.error('Erreur générale :', e);
