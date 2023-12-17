21e327455c94b7739c929da52755cbe799bd6c8f
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-17 11:43:30 +0000

    Update voir.js

--- commandes/voir.js
+++ commandes/voir.js
@@ -20,19 +20,18 @@ zokou({ nomCom: "voir", categorie: "Général", reaction: "🤲🏿" }, async (d
       var texte = msgRepondu.viewOnceMessageV2.message.videoMessage.caption;
 
       await zk.sendMessage(dest, { video: { url: video }, caption: texte }, { quoted: ms });
-    } else if (msgRepondu.viewOnceMessageV2.message.audioMessage) {
-      var audio = await zk.downloadAndSaveMediaMessage(msgRepondu.viewOnceMessageV2.message.audioMessage);
-      const filename = `${Math.random().toString(36)}`;
-      let ran = `${filename}.mp3`;
-      let va = fs.unlinkSync(media);
-      let buff1 = fs.readFileSync(ran);
-
-      await zk.sendMessage(
-                      dest,
-                      { audio: buff1, mimetype: "audio/mpeg" },
-                      { quoted: ms }
-                    );
-                    fs.unlinkSync(ran);
+     } else if (msgRepondu.viewOnceMessageV2.message.audioMessage) {
+        var audio = await zk.downloadAndSaveMediaMessage(msgRepondu.viewOnceMessageV2.message.audioMessage);
+        const filename = `${Math.random().toString(36)}`;
+        let ran = `${filename}.mp3`;
+        fs.unlinkSync(audio); // Suppression du fichier audio
+        let buff1 = fs.readFileSync(ran);
+        await zk.sendMessage(
+          dest,
+          { audio: buff1, mimetype: "audio/mpeg" },
+          { quoted: ms }
+        );
+        fs.unlinkSync(ran); 
     } else {
       return repondre("Le message que vous avez mentionné n'est pas un message envoyé en vue unique.");
     }
