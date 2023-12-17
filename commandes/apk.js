85286803907d974f7117afe8386078d0a128b095
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-17 14:28:09 +0000

    Update apk.js

--- commandes/apk.js
+++ commandes/apk.js
@@ -1,101 +1,69 @@
 const { zokou } = require("../framework/zokou");
+const { search, download } = require('aptoide-scraper');
 const axios = require('axios');
+const fs = require('fs');
 
-
-
-zokou({ nomCom: "gpt", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
-  const { repondre, arg } = commandeOptions;
+zokou({ nomCom: "app", reaction: "✨", categorie: "Recherche" }, async (dest, zk, commandeOptions) => {
+  const { repondre, arg, origineMessage, ms } = commandeOptions;
 
   if (!arg || arg.length === 0) {
     return repondre("Veuillez entrer le nom de l'application à rechercher.");
   }
-
+  
+  const getRandomName = (ext) => `${Math.floor(Math.random() * 10000)}${ext}`;
+  const randomName = getRandomName(".app");
+  const filePath = `./${randomName}`;
   const nom = arg.join(' ');
 
   try {
     const recherche = search(nom);
-    let data = {};
-    if(recherche.length) { 
-    let data = await download(recherche[0].id); 
-      
-  } else { return repondre(`l'application rechercher est introuvable`)};
 
-    const appinfo = parseInt(data.size);
-	if(appinfo > 200) return repondre("impossible de telecharger une application de plus de 200Mb );
-       const url = data.dllink;
-	 let info ="*Nom de l'application:* " +data.name;
-         info +="\n*Identifiant de l'application:* " +data.package;
-         info +="\n*Derniere mise a jour:* " +data.lastup;
-         info +="\n*Taille de l'application:* " +data.size;
-        // info +="\n*Lien de l'application:* " +data.dllink;
-	info +="\n\n "
-	
-  } catch (e) {
-    console.error('Erreur générale :', e);
-    repondre("Oups, une erreur est survenue lors du traitement de votre demande.");
-  }
-});
+    if (recherche.length === 0) {
+      return repondre(`L'application recherchée est introuvable.`);
+    }
 
+    const data = await download(recherche[0].id);
+    const appSize = parseInt(data.size);
 
+    if (appSize > 200) {
+      return repondre("Impossible de télécharger une application de plus de 200 Mo.");
+    }
 
-attern: "apk",
-            desc: "Downloads apks  .",
-            category: "downloader",
-            filename: __filename,
-            use: '<add sticker url.>',
-        },
+    const url = data.dllink;
 
-        async(Void, citel, text) => {
-        if(!text )return citel.reply("*Give me App Name*");
-
-	const getRandom = (ext) => { return `${Math.floor(Math.random() * 10000)}${ext}`; };
-	let randomName = getRandom(".apk");
-	const filePath = `./${randomName}`;     // fs.createWriteStream(`./${randomName}`)
-        const {  search , download } = require('aptoide-scraper')
-	let searc = await search(text);          //console.log(searc);
-	let data={};
-	if(searc.length){ data = await download(searc[0].id); }
-	else return citel.send("*APP not Found, Try Other Name*");
-	
-	
-	const apkSize = parseInt(data.size);
-	if(apkSize > 150) return citel.send(`❌ File size bigger than 200mb.`);
-       const url = data.dllink;
-	 let  inf  ="*App Name :* " +data.name;
-         inf +="\n*App id        :* " +data.package;
-         inf +="\n*Last Up       :* " +data.lastup;
-         inf +="\n*App Size     :* " +data.size;
-        // inf +="\n*App Link     :* " +data.dllink;
-	inf +="\n\n "
-         
-
-axios.get(url, { responseType: 'stream' })
-  .then(response => {
     const writer = fs.createWriteStream(filePath);
-    response.data.pipe(writer);
+    const response = await axios({
+      url,
+      method: 'GET',
+      responseType: 'stream',
+    });
 
-    return new Promise((resolve, reject) => {
+    response.data.pipe(writer);
+    await new Promise((resolve, reject) => {
       writer.on('finish', resolve);
       writer.on('error', reject);
     });
-  }).then(() => {
-	
-	let buttonMessage = {
-                        document: fs.readFileSync(filePath),
-                        mimetype: 'application/vnd.android.package-archive',
-                        fileName: data.name+`.apk`,
-                        caption : inf
-                        
-                    }
-                  Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
 
-    console.log('File downloaded successfully');
+    const rep2 = {
+      document: fs.readFileSync(filePath),
+      mimetype: 'application/vnd.android.package-archive',
+      fileName: `${data.name}.app`,
+      caption: `*Nom de l'application :* ${data.name}\n` +
+               `*Identifiant de l'application :* ${data.package}\n` +
+               `*Dernière mise à jour :* ${data.lastup}\n` +
+               `*Taille de l'application :* ${data.size}\n\n`,
+    };
 
-  
-    fs.unlink(filePath, (err) => {
-      if (err) { console.error('Error deleting file:', err); } else { console.log('File deleted successfully'); } });
-  }) .catch(error => {
-	fs.unlink(filePath)
-    return citel.reply('*Apk not Found, Sorry*')//:', error.message);
-  });
-}
+    await zk.sendMessage(origineMessage, rep2, { quoted: ms });
+
+    console.log('Application téléchargée avec succès.');
+
+    await fs.unlink(filePath);
+
+    console.log('Application supprimée avec succès.');
+  } catch (e) {
+    console.error('Erreur générale :', e);
+    repondre("Oups, une erreur est survenue lors du téléchargement de l'application.");
+  }
+});
+
