cc36ebbbf004a35bcb680a641d0552ca29d409aa
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-25 17:35:52 +0000

    Update fiche.js

--- bdd/fiche.js
+++ bdd/fiche.js
@@ -76,10 +76,7 @@ async function ajouterOuMettreAJourUserData(messageEtValeur) {
   const object = arg.join'';
   let avtmsg = 'JOUER: ${jouer} actualise ${object} +/- ${valeur}'
 
-    if (message.message.textmessage = avtmsg) {
-
-      // Exemple pour NEO PASS
-      const rege
+    if (MsgRepondu.message.textmessage = avtmsg) {
 
     } else {
       // Si le JID n'existe pas, ajoutez-le avec des valeurs par défaut
@@ -98,7 +95,7 @@ async function getR {
 
   try {
     // Sélectionnez les valeurs pour le JID donné
-    const query = 'SELECT R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, R35, R36, R37, R38, R39 FROM users_fiche;
+    const query = 'SELECT R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, R35, R36, R37, R38, R39 FROM users_fiche';
     const result = await client.query(query);
 
     if (result.rows.length > 0) {
