0bbe279c220d0c88c9a06f67ca29d022e6600399
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-25 20:18:16 +0000

    Update fiche.js

--- bdd/fiche.js
+++ bdd/fiche.js
@@ -174,8 +174,8 @@ async function getR() {
 
     if (result.rows.length > 0) {
       // Retournez les valeurs
-      const { R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20 }
-      return result.rows[0];
+      const { R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, R35, R36, R37, R38, R39 } = result.rows[0];
+       return { R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, R35, R36, R37, R38, R39 };
     } else {
       // Si le JID n'existe pas, renvoyez des valeurs par défaut
       console.error('Utilisateur non trouvé');
@@ -196,11 +196,3 @@ module.exports = {
   getR,
 };
 
-
-
-    if (result.rows.length > 0) {
-      // Retournez les valeurs de messages et d'XP
-      const { messages, xp } = result.rows[0];
-      return { messages, xp };
-    } else {
-      // Si le JID n'existe
