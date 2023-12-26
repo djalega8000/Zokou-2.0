257e52ddf0c3ae3c22e5dee82e07cb99fdd201ea
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-26 18:01:48 +0000

    Update fiche.js

--- bdd/fiche.js
+++ bdd/fiche.js
@@ -161,7 +161,7 @@ async function actualiserFicheUtilisateur(ms) {
 
         if (colonneObjet) {
           // Mettez à jour la table avec les valeurs extraites du message
-          await client.query(`UPDATE users_fiche SET ${colonneObjet} = ${colonneObjet} + $1`, [valeur]);
+          await client.query(`UPDATE users_fiche SET ${colonneObjet} = ${colonneObjet} + $1`, [valeur] WHERE id = 1);
           console.log(`données de l'utilisateur ${joueur} mises à jour`);
         } else {
           console.log("Nom d'objet non reconnu.");
@@ -183,7 +183,7 @@ async function getR() {
   try {
     // Sélectionnez les valeurs pour tous les joueurs
     const query =
-      "SELECT R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, R35, R36, R37, R38, R39 FROM users_fiche";
+      "SELECT R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, R35, R36, R37, R38, R39 FROM users_fiche WHERE id = 1";
     const result = await client.query(query);
     const {
         R1,
