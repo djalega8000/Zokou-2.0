833cdbfa57aa5078426623081b033206c4834e59
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-27 09:11:29 +0000

    Update fiche.js

--- bdd/fiche.js
+++ bdd/fiche.js
@@ -22,52 +22,51 @@ async function createUsersFicheTable() {
     // Créez la table users_fiche si elle n'existe pas déjà
     await client.query(`
       CREATE TABLE IF NOT EXISTS users_fiche(
-        id serial PRIMARY KEY,
         -- Joueur 1 (Lily KÏNGS II)
-        R1 INTEGER DEFAULT 0,
+        R1 INTEGER DEFAULT 10000,
         R2 TEXT DEFAULT 'aucun',
-        R3 INTEGER DEFAULT 0,
-        R4 INTEGER DEFAULT 0,
-        R5 INTEGER DEFAULT 0,
-        R6 INTEGER DEFAULT 0,
-        R7 INTEGER DEFAULT 0,
-        R8 INTEGER DEFAULT 0,
-        R9 INTEGER DEFAULT 0,
-        R10 INTEGER DEFAULT 0,
-        R11 INTEGER DEFAULT 0,
-        R12 INTEGER DEFAULT 0,
+        R3 INTEGER DEFAULT 10000,
+        R4 INTEGER DEFAULT 10000,
+        R5 INTEGER DEFAULT 10000,
+        R6 INTEGER DEFAULT 10000,
+        R7 INTEGER DEFAULT 10000,
+        R8 INTEGER DEFAULT 10000,
+        R9 INTEGER DEFAULT 10000,
+        R10 INTEGER DEFAULT 10000,
+        R11 INTEGER DEFAULT 10000,
+        R12 INTEGER DEFAULT 10000,
         R13 TEXT DEFAULT 'aucun',
         -- ... (ajoutez les colonnes spécifiques à Lily)
 
         -- Joueur 2 (DAMIEN KÏNGS III)
-        R14 INTEGER DEFAULT 0,
+        R14 INTEGER DEFAULT 10000,
         R15 TEXT DEFAULT 'aucun', 
-        R16 INTEGER DEFAULT 0,
-        R17 INTEGER DEFAULT 0,
-        R18 INTEGER DEFAULT 0,
-        R19 INTEGER DEFAULT 0,
-        R20 INTEGER DEFAULT 0,
-        R21 INTEGER DEFAULT 0,
-        R22 INTEGER DEFAULT 0,
-        R23 INTEGER DEFAULT 0,
-        R24 INTEGER DEFAULT 0,
-        R25 INTEGER DEFAULT 0,
+        R16 INTEGER DEFAULT 10000,
+        R17 INTEGER DEFAULT 10000,
+        R18 INTEGER DEFAULT 10000,
+        R19 INTEGER DEFAULT 10000,
+        R20 INTEGER DEFAULT 10000,
+        R21 INTEGER DEFAULT 10000,
+        R22 INTEGER DEFAULT 10000,
+        R23 INTEGER DEFAULT 10000,
+        R24 INTEGER DEFAULT 10000,
+        R25 INTEGER DEFAULT 10000,
         R26 TEXT DEFAULT 'aucun',
         -- ... (ajoutez les colonnes spécifiques à DAMIEN)
 
         -- Joueur 3 (Kanzen Gold King)
-        R27 INTEGER DEFAULT 0,
+        R27 INTEGER DEFAULT 10000,
         R28 TEXT DEFAULT 'aucun', 
-        R29 INTEGER DEFAULT 0,
-        R30 INTEGER DEFAULT 0,
-        R31 INTEGER DEFAULT 0,
-        R32 INTEGER DEFAULT 0,
-        R33 INTEGER DEFAULT 0,
-        R34 INTEGER DEFAULT 0,
-        R35 INTEGER DEFAULT 0,
-        R36 INTEGER DEFAULT 0,
-        R37 INTEGER DEFAULT 0,
-        R38 INTEGER DEFAULT 0,
+        R29 INTEGER DEFAULT 10000,
+        R30 INTEGER DEFAULT 10000,
+        R31 INTEGER DEFAULT 10000,
+        R32 INTEGER DEFAULT 10000,
+        R33 INTEGER DEFAULT 10000,
+        R34 INTEGER DEFAULT 10000,
+        R35 INTEGER DEFAULT 10000,
+        R36 INTEGER DEFAULT 10000,
+        R37 INTEGER DEFAULT 10000,
+        R38 INTEGER DEFAULT 10000,
         R39 TEXT DEFAULT 'aucun'
         -- ... (ajoutez les colonnes spécifiques à Kanzen)
       );
@@ -82,18 +81,16 @@ async function createUsersFicheTable() {
 
 async function actualiserFicheUtilisateur(ms) {
   const client = await pool.connect();
-
-await client.query(`INSERT INTO users_fiche (id), value(1)`;
-  console.log('id cree avec sucess')
   
   try {
     const baileys_1 = require("@sampandey001/baileys");
     var mtype = baileys_1.getContentType(ms.message);
-    var texte = mtype == "conversation" ? ms.message.conversation : "";
+    var texte = mtype == "conversation" ms.message.conversation : "";
 
-    var msg = /JOUER: (\w+) actualise (\w+) \+\/- (\d+)/;
+    const msg = /JOUER: (\w+) actualise (\w+) \+\/- (\d+)/;
 
-    if (console.log(texte) == msg) {
+    if (msg.test(texte)) {
+      console.log('message au format attendu');
       const match = texte.match(msg);
 
       if (match) {
@@ -164,7 +161,7 @@ await client.query(`INSERT INTO users_fiche (id), value(1)`;
 
         if (colonneObjet) {
           // Mettez à jour la table avec les valeurs extraites du message
-          await client.query(`UPDATE users_fiche SET ${colonneObjet} = ${colonneObjet} + $1 WHERE id = 1`,[valeur]);
+          await client.query(`UPDATE users_fiche SET ${colonneObjet} = ${colonneObjet} + $1`,[valeur]);
           console.log(`données de l'utilisateur ${joueur} mises à jour`);
         } else {
           console.log("Nom d'objet non reconnu.");
@@ -186,7 +183,7 @@ async function getR() {
   try {
     // Sélectionnez les valeurs pour tous les joueurs
     const query =
-      "SELECT R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, R35, R36, R37, R38, R39 FROM users_fiche WHERE id = 1";
+      "SELECT R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, R35, R36, R37, R38, R39 FROM users_fiche";
     const result = await client.query(query);
     const {
         R1,
