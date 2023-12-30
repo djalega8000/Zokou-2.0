6d6d999bdfd13c3d57767fbae5328eec518b01f6
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-30 09:29:48 +0000

    Update neo.js

--- commandes/neo.js
+++ commandes/neo.js
@@ -88,6 +88,7 @@ const { ms, repondre, arg } = commandeOptions;
 
 const client = await pool.connect();
 const argsArray = ar.split(" ");
+        repondre(argsArray);
 const arg1 = argsArray[0];
 const arg2 = argsArray[1];
 const arg3 = argsArray[2];
