4b0657db8af59a0dcca635bdbcb081e1ee83d73d
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-31 00:06:04 +0000

    Create east1fiche.js

---
+++ commandes/east1fiche.js
@@ -0,0 +1,51 @@
+ . 𝗘𝗔𝗦𝗧 𝗗𝗜𝗩𝗜𝗦𝗜𝗢𝗡🦅🟢
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+◇ *Pseudo👤*: Atsushi KÏNGS🇨🇲 
+◇ *Rang XP🔰*: ${r1}
+◇ *Golds🧭*: ${r2}🧭
+◇ *NEOcoins🔹*: 13🔷
+◇ *Gift Box🎁*: 0🎁
+◇ *Coupons🎟️*: 0🎟️
+◇ *NEO PASS🔸*: 0🔸
+░░░░░░░░░░░░░░
+*Note*: ⭐1/5
+*Records*: 25 Victoires✅/ 67 Défaites❌
+*🏆Trophées*: 0 *🌟TOS*: 0 *💫Awards*: 0
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+*🎴Cards(15 max)*: Borushiki 
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+
+◇ *Pseudo👤*: White KÏNGS🇨🇮 
+◇ *Rang XP🔰*: *SUPERSTAR💫*: Grade A
+◇ *Golds🧭*: 2.025.000🧭
+◇ *NEOcoins🔹*: 30🔷
+◇ *Gift Box🎁*: 1🎁
+◇ *Coupons🎟️*: 0🎟️
+◇ *NEO PASS🔸*: 0🔸
+░░░░░░░░░░░░░░
+*Note*: ⭐⭐⭐⭐4/5
+*Records*: 62 Victoires✅/ 30 Défaites❌
+*🏆Trophées*: 1 *🌟TOS*: 2 *💫Awards*: 0
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+*🎴Cards(15 max)*: Sukuna, Saitama. 
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+
+◇ *Pseudo👤*: Kemael🇨🇮
+◇ *Rang XP🔰*: Joueur Classé🎮
+◇ *Golds🧭*: 130.000🧭
+◇ *NEOcoins🔹*: 3🔷
+◇ *Gift Box🎁*: 1🎁
+◇ *Coupons🎟️*: 100🎟️
+◇ *NEO PASS🔸*: 0🔸
+░░░░░░░░░░░░░░
+*Note*: ⭐⭐2.5/5
+*Records*: 17 Victoires✅/ 6 Défaites❌
+*🏆Trophées*: 1 *🌟TOS*: 1 *💫Awards*: 0
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+*🎴Cards(15 max)*: Itadori, Natsu, Goku(début), Goku SSJ2
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+ *◁🔷𝗡𝗘𝗢 𝗙𝗢𝗥 𝗧𝗛𝗘 𝗣𝗟𝗔𝗬𝗘𝗥𝗦🎮➕ᐅᐭ*
