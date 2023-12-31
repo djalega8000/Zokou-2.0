a714f2dbfd57d10fe19fb5bf14f70bdfa6a1ab56
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2023-12-31 17:06:25 +0000

    Update fiche.js

--- bdd/fiche.js
+++ bdd/fiche.js
@@ -81,7 +81,7 @@ async function getR() {
   try {
     // Sélectionnez les valeurs pour tous les joueurs
     const query =
-      "SELECT r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36, r37, r38, r39 FROM tex_fiche WHERE id = 1";
+      "SELECT r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36, r37, r38, r39 FROM tex_fiche";
     const result = await client.query(query);
     const {
       r1,
