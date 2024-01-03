3c53614254e1bcaca04a4f71fc3d2c0cd8a2d15f
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-01-03 14:04:19 +0000

    Update north1fiche.js

--- commandes/north1fiche.js
+++ commandes/north1fiche.js
@@ -7,8 +7,10 @@ zokou(
     categorie: 'NEOverse'
   },
   async (dest, zk, commandeOptions) => {
-    const { ms, repondre } = commandeOptions;
+    const { ms, repondre, arg } = commandeOptions;
+    try{
     const data = await getR();
+    if(!arg && arg.length === 0) {
     let mesg = `.*𝗡𝗢𝗥𝗧𝗛 𝗗𝗜𝗩𝗜𝗦𝗜𝗢𝗡🐺🔴*
 ░░░░░░░░░░░░░░░░░░░
 ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
@@ -61,4 +63,96 @@ Records: 0 Victoires✅/ 0 Défaites❌
 ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
          *◁🔷𝗡𝗘𝗢 𝗙𝗢𝗥 𝗧𝗛𝗘 𝗣𝗟𝗔𝗬𝗘𝗥𝗦🎮➕ᐅᐭ*`;
 zk.sendMessage(dest, { image: { url: 'https://i.imgur.com/UP1ubll.jpg' }, caption: mesg }, { quoted: ms });
+  } else {	
+    var dbUrl = "postgresql://postgres:aga-B533E3BcGdfa5*cFf*4daE4*f*fB@monorail.proxy.rlwy.net:12102/railway";	
+    const proConfig = {	
+      connectionString: dbUrl,	
+      ssl: {	
+        rejectUnauthorized: false,	
+      },	
+    };	
+
+    const { Pool } = require('pg');	
+    const pool = new Pool(proConfig);	
+    const client = await pool.connect();	
+      if(arg && arg[0]('joueur')) {
+
+      let joueur = arg[1]('');
+      let object = arg[3]('');
+      let signe = arg[4]('');
+      let valeur = arg[5]('');
+let colonnesJoueur;	
+
+  switch (joueur) {	
+    case "lily":	
+      colonnesJoueur = {	
+        rang_xp: "r1",	
+        golds: "r2",	
+        neocoins: "r3",	
+        gift_box: "r4",	
+        coupons: "r5",	
+        neo_pass: "r6",	
+        victoires: "r7",	
+        defaites: "r8",	
+        trophees: "r9",	
+        tos: "r10",	
+        awards: "r11",	
+        cards: "r12",	
+      };	
+      break;	
+    case "damien":	
+      colonnesJoueur = {	
+        rang_xp: "r13",	
+        golds: "r14",	
+        neocoins: "r15",	
+        gift_box: "r16",	
+        coupons: "r17",	
+        neo_pass: "r18",	
+        victoires: "r19",	
+        defaites: "r20",	
+        trophees: "r21",	
+        tos: "r22",	
+        awards: "r23",	
+        cards: "r24",	
+      };	
+      break;	
+    default:	
+      console.log("Nom de joueur non reconnu.");	
+      repondre(`joueur: ${joueur} non reconnu`);
+      return;	
+  }	
+
+    const colonneObjet = colonnesJoueur[object];	
+
+  if (colonneObjet) {	
+      await client.query(UPDATE north1fiche_fiche SET ${colonneObjet} = ${colonneObjet} ${signe} ${valeur});	
+      console.log(`Données de l'utilisateur ${joueur} mises à jour`);	
+      repondre(`Données du joueur ${joueur} mises à jour`);	
+  } else {	
+    console.log("Nom d'objet non reconnu.");	
+    repondre(`Une erreur est survenue. Veuillez entrer correctement les données.`);	
+  }	
+} else {	
+  console.log("Le message ne correspond pas au format attendu.");	
+  repondre(`Le format du message est incorrect.`);	
+}	
+}	
+  } catch (error) {	
+    console.error("Erreur lors de la mise à jour des données de l'utilisateur:", error);	
+    repondre(`une erreur est survenu lors de la mise a jouer des données du jouer ${joueur}`);	
+  } finally {	
+          var dbUrl = "postgresql://postgres:aga-B533E3BcGdfa5*cFf*4daE4*f*fB@monorail.proxy.rlwy.net:12102/railway";	
+    const proConfig = {	
+      connectionString: dbUrl,	
+      ssl: {	
+        rejectUnauthorized: false,	
+      },	
+    };	
+    	
+    const { Pool } = require('pg');	
+    const pool = new Pool(proConfig);	
+
+    const client = await pool.connect();	
+    client.release();	
+  }	
 });
