66a0c1ed5462b4a79a5c2e9bc5f547ec805a9deb
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-01-04 09:30:02 +0000

    Update north1fiche.js

--- bdd/north1fiche.js
+++ bdd/north1fiche.js
@@ -16,49 +16,49 @@ async function createNorth1FicheTable() {
   try {
     // Créez la table north1_fiche si elle n'existe pas déjà
     await client.query(`
-      CREATE TABLE IF NOT EXISTS north4_fiche(
+      CREATE TABLE IF NOT EXISTS north4_iche(
         id SERIAL PRIMARY KEY,
-        r1 text,
-        r2 text,
-        r3 text,
-        r4 text,
-        r5 text,
-        r6 text,
-        r7 text,
-        r8 text,
-        r9 text,
-        r10 text,
-        r11 text,
-        r12 text,
-        r13 text,
-        r14 text,
-        r15 text, 
-        r16 text,
-        r17 text,
-        r18 text,
-        r19 text,
-        r20 text,
-        r21 text,
-        r22 text,
-        r23 text,
-        r24 text,
-        r25 text,
-        r26 text,
-        r27 text,
-        r28 text, 
-        r29 text,
-        r30 text,
-        r31 text,
-        r32 text,
-        r33 text,
-        r34 text,
-        r35 text,
-        r36 text
+        r1 TEXT,
+        r2 INTEGER,
+        r3 INTEGER,
+        r4 INTEGER,
+        r5 INTEGER,
+        r6 INTEGER,
+        r7 INTEGER,
+        r8 INTEGER,
+        r9 INTEGER,
+        r10 INTEGER,
+        r11 INTEGER,
+        r12 TEXT,
+        r13 TEXT,
+        r14 INTEGER,
+        r15 INTEGER, 
+        r16 INTEGER,
+        r17 INTEGER,
+        r18 INTEGER,
+        r19 INTEGER,
+        r20 INTEGER,
+        r21 INTEGER,
+        r22 INTEGER,
+        r23 INTEGER,
+        r24 TEXT,
+        r25 TEXT,
+        r26 INTEGER,
+        r27 INTEGER,
+        r28 INTEGER, 
+        r29 INTEGER,
+        r30 INTEGER,
+        r31 INTEGER,
+        r32 INTEGER,
+        r33 INTEGER,
+        r34 INTEGER,
+        r35 INTEGER
+        r36 TEXT
       );
     `);
-    console.log('Table north4_fiche créée avec succès');
+    console.log('Table north4_iche créée avec succès');
   } catch (error) {
-    console.error('Erreur lors de la création de la table north4_fiche:', error);
+    console.error('Erreur lors de la création de la table north4_iche:', error);
   } finally {
     client.release();
   }
@@ -68,8 +68,8 @@ async function insererValeur() {
   const client = await pool.connect();
   try {
     const query = `
-      INSERT INTO north4_fiche(id, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36)
-      VALUES (1, 0, 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 0, 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 0, 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun')`;
+      INSERT INTO north4_iche(id, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36)
+      VALUES (1,'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun')`;
     await client.quety(query);
     console.log('Valeur ajoutée avec succès si les colonnes étaient vides');
   } catch (error) {
@@ -85,7 +85,7 @@ async function getR() {
   try {
     // Sélectionnez les valeurs pour tous les joueurs
     const query =
-      "SELECT r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36 FROM north4_fiche WHERE id = 1";
+      "SELECT r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36 FROM north4_iche WHERE id = 1";
     const result = await client.query(query);
     const {
       r1,
