d4fc9c6dddaa011b58d381331db2c3fae37e1a3f
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-01-04 09:48:32 +0000

    Update north1fiche.js

--- bdd/north1fiche.js
+++ bdd/north1fiche.js
@@ -70,7 +70,7 @@ async function insererValeur() {
     const query = `
       INSERT INTO north4_iche(id, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36)
       VALUES (1,'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun')`;
-    await client.quety(query);
+    await client.query(query);
     console.log('Valeur ajoutée avec succès si les colonnes étaient vides');
   } catch (error) {
     console.error('Erreur lors de l\'ajout des données', error);
