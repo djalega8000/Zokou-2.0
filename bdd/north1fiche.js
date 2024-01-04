29a4dceda2baaaa9ba4bed4ea4c44780bcf99ca9
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-01-04 01:24:35 +0000

    Update north1fiche.js

--- bdd/north1fiche.js
+++ bdd/north1fiche.js
@@ -67,9 +67,10 @@ async function createNorth1FicheTable() {
 async function insererValeur() { 
   const client = await pool.connect();
   try {
-    await client.query(`
+    const query = `
       INSERT INTO north4_fiche(id, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36)
-      VALUES ('1', 0, 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 0, 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 0, 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun')`);
+      VALUES (1, 0, 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 0, 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 0, 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun')`;
+    await client.quety(query);
     console.log('Valeur ajoutée avec succès si les colonnes étaient vides');
   } catch (error) {
     console.error('Erreur lors de l\'ajout des données', error);
