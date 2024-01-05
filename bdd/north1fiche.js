db6b787ab136cdb59a44e522f857efeb6252eac3
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-01-05 20:49:03 +0000

    Update north1fiche.j

--- bdd/north1fiche.js
+++ bdd/north1fiche.js
@@ -19,67 +19,45 @@ async function createNorth1FicheTable() {
       CREATE TABLE IF NOT EXISTS north4_che(
         id SERIAL PRIMARY KEY,
         r1 TEXT DEFAULT 'aucun',
-        r2 INTEGER,
-        r3 INTEGER,
-        r4 INTEGER,
-        r5 INTEGER,
-        r6 INTEGER,
-        r7 INTEGER,
-        r8 INTEGER,
-        r9 INTEGER,
-        r10 INTEGER,
-        r11 INTEGER,
+        r2 INTEGER DEFAULT 0,
+        r3 INTEGER DEFAULT 0,
+        r4 INTEGER DEFAULT 0,
+        r5 INTEGER DEFAULT 0,
+        r6 INTEGER DEFAULT 0,
+        r7 INTEGER DEFAULT 0,
+        r8 INTEGER DEFAULT 0,
+        r9 INTEGER DEFAULT 0,
+        r10 INTEGER DEFAULT 0,
+        r11 INTEGER DEFAULT 0,
         r12 TEXT DEFAULT 'aucun',
         r13 TEXT DEFAULT 'aucun',
-        r14 INTEGER,
-        r15 INTEGER, 
-        r16 INTEGER,
-        r17 INTEGER,
-        r18 INTEGER,
-        r19 INTEGER,
-        r20 INTEGER,
-        r21 INTEGER,
-        r22 INTEGER,
-        r23 INTEGER,
+        r14 INTEGER DEFAULT 0,
+        r15 INTEGER DEFAULT 0,
+        r16 INTEGER DEFAULT 0,
+        r17 INTEGER DEFAULT 0,
+        r18 INTEGER DEFAULT 0,
+        r19 INTEGER DEFAULT 0,
+        r20 INTEGER DEFAULT 0,
+        r21 INTEGER DEFAULT 0,
+        r22 INTEGER DEFAULT 0,
+        r23 INTEGER DEFAULT 0,
         r24 TEXT DEFAULT 'aucun',
         r25 TEXT DEFAULT 'aucun',
-        r26 INTEGER,
-        r27 INTEGER,
-        r28 INTEGER, 
-        r29 INTEGER,
-        r30 INTEGER,
-        r31 INTEGER,
-        r32 INTEGER,
-        r33 INTEGER,
-        r34 INTEGER,
-        r35 INTEGER,
+        r26 INTEGER DEFAULT 0,
+        r27 INTEGER DEFAULT 0,
+        r28 INTEGER DEFAULT 0,
+        r29 INTEGER DEFAULT 0,
+        r30 INTEGER DEFAULT 0,
+        r31 INTEGER DEFAULT 0,
+        r32 INTEGER DEFAULT 0,
+        r33 INTEGER DEFAULT 0,
+        r34 INTEGER DEFAULT 0,
+        r35 INTEGER DEFAULT 0,
         r36 TEXT DEFAULT 'aucun'
     `);
     console.log('Table north4_iche créée avec succès');
   } catch (error) {
     console.error('Erreur lors de la création de la table north4_iche:', error);
-  } finally {
-    client.release();
-  }
-}
-
-async function insererValeur() { 
-  const client = await pool.connect();
-  try {
-    // Sélectionnez les valeurs pour tous les joueurs
-    const query =
-      "SELECT id, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36 FROM north4_che WHERE id = 1";
-    const result = await client.query(query);
-    if (result.row <0)  {
-  
-  const insertQuery = `
-      INSERT INTO north4_che(id, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36)
-      VALUES (1,'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun')`;
-    await client.query(insertQuery);
-
-    console.log('Valeur ajoutée avec succès si les colonnes étaient vides');
-  } catch (error) {
-    console.error('Erreur lors de l\'ajout des données', error);
   } finally {
     client.release();
   }
