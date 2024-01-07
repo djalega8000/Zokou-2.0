bbf6ba44e42da13f39f571dec5c042e28b5bf6a6
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-01-07 11:56:07 +0000

    Update gpt.js

--- commandes/gpt.js
+++ commandes/gpt.js
@@ -36,6 +36,42 @@ zokou({ nomCom: "gpt", reaction: "📡", categorie: "IA" }, async (dest, zk, com
   } catch (error) {
     console.error('Erreur:', error.message || 'Une erreur s\'est produite');
     repondre("Oups, une erreur est survenue lors du traitement de votre demande.");
+  });
+
+zokou({ nomCom: "dalle", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+  const { repondre, arg, ms } = commandeOptions;
+
+  try {
+    if (!arg || arg.length === 0) {
+      return repondre(`Veuillez entrer les informations nécessaires pour générer l'image.`);
+    }
+
+    const image = arg.join(' ');
+    const imageSize = '256x256';
+    const apiUrl = 'https://api.openai.com/v1/images/generations';
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${conf.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'image-alpha-001',
+        prompt: image,
+        size: imageSize,
+        response_format: 'url',
+      }),
+    });
+
+    const data = await response.json();
+    let caption = '*Propulsé par ZOKOU-MD*';
+    if (!data.choices || data.choices.length === 0) {
+      repondre("OPENAI_API_KEY  invalide, veuillez mettre une nouvelle clé");
+    } else {
+    zk.sendMessage(dest, { image: { url: data, caption: caption }, quoted: ms });
+    }
+  } catch (error) {
+    console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+    repondre("Oups, une erreur est survenue lors du traitement de votre demande.");
   }
 });
-
