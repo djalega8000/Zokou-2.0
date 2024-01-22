fede2e22d083ad5862b47a2d41e7f031d416939e
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-01-22 18:10:15 +0000

    Update set.js

--- set.js
+++ set.js
@@ -21,7 +21,7 @@ module.exports = { session: process.env.SESSION_ID || 'zokk',
     HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
     HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
     WARN_COUNT : process.env.WARN_COUNT || '3' ,
-    GPT : process.env.OPENAI_API_KEY,
+    //GPT : process.env.OPENAI_API_KEY,
     DP : process.env.STARTING_BOT_MESSAGE,              
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
